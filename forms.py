02c4d2d9af5f820f2a38baa309af4f0dba79b145
Me <mikrfos@gmail.com>
2022-10-10 20:39:29 -0400

    Added form and validation to Create to-do-list. TODO- Find a way to check off sub-objectives individually.

---
+++ forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, URL, Optional
+
+
+class ToDoListForm(FlaskForm):
+    title = StringField("List Title", validators=[DataRequired()])
+    image_url = StringField("Image URL", validators=[DataRequired(), URL()])
+    sub_objectives = StringField("List Sub-Objectives", render_kw={"placeholder":"Separate with double comma (,,)"},
+                                 validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+class ObjectiveForm(FlaskForm):
+    check = BooleanField(validators=[Optional()])
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, url_for
+import os
+
+from flask import Flask, render_template, url_for, redirect
 from flask_login import LoginManager
+
+from forms import ToDoListForm, ObjectiveForm
 from model import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///to-do-list.db"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 db.init_app(app)
+data = []
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+    form = ToDoListForm()
+    if form.validate_on_submit():
+        sub_objectives = [[obj, ObjectiveForm()] for obj in form.sub_objectives.data.split(",,")]
+        to_do_list = {
+            "Title": form.title.data,
+            "img_url": form.image_url.data,
+            "Sub_Objectives": sub_objectives,
+            "progress": 0,
+        }
+        data.append(to_do_list)
+    return render_template("index.html", form=form, data=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
